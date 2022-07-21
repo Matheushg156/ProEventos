805f135f3b3924d9877ccb19863550cc0a4a18dd
Matheus Henrique Gonzaga <matheushgrdc156@gmail.com>
2022-07-21 12:25:38 -0300

    Adiciona filtros e banco de dados a pagina

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  public filteredEventos: any = [];
+  widthImg: number = 150;
+  marginImg: number = 2;
+  showImg: boolean = true;
+  private _filterList: string = '';
+
+  public get filterList(): string {
+    return this._filterList;
+  }
+
+  public set filterList(value: string) {
+    this._filterList = value;
+    this.filteredEventos = this._filterList ? this.filterEventos(this._filterList) : this.eventos;
+  }
+
+  public filterEventos(filterBy: string): any {
+    filterBy = filterBy.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: any) => evento.tema.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      evento.local.toLocaleLowerCase().indexOf(filterBy) !== -1 ||
+      evento.dataEvento.toLocaleLowerCase().indexOf(filterBy) !== -1
+      );
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -16,10 +39,15 @@ export class EventosComponent implements OnInit {
     this.getEventos();
   }
 
+  showngImage(): void {
+    this.showImg = !this.showImg;
+  }
+
   public getEventos(): void {
     this.http.get('https://localhost:5001/api/eventos').subscribe(
       data => {
         this.eventos = data;
+        this.filteredEventos = this.eventos;
       },
       error => {
         console.log(error);
